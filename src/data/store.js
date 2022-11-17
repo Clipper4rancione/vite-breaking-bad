3ba728b859b93bd2bea2c2beabc689f9e3aa1986
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-11-17 19:04:57 +0100

    created store

--- src/data/store.js
+++ src/data/store.js
@@ -2,5 +2,6 @@ import { reactive } from "vue";
 
 
 export const store = reactive({
-    apiUrlBrBa: "https://www.breakingbadapi.com/api/characters"
+    apiUrlBrBa: "https://www.breakingbadapi.com/api/characters",
+    castList: []
 });
