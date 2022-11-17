3cbc03f5dc1ed5efb76de53d81c59ae505f57f9e
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-11-17 16:31:13 +0100

    [add] appHeader and elements

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header>
+    <div class="container d-flex align-items-center">
+      <div class="logo">
+        <img src="/public/Breaking_Bad_logo.svg" alt="" />
+      </div>
+      <h1>Breaking Bad API Cast</h1>
+    </div>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+header {
+  padding: 20px;
+}
+.logo {
+  width: 130px;
+}
+img {
+  width: 100%;
+  vertical-align: middle;
+}
+h1 {
+  color: white;
+  margin-left: 50px;
+}
+</style>
