6c3a8ecfedcf7fcacfb1e46f9b65483717408d5d
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-11-17 17:51:18 +0100

    [add] API call

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <script>
+import axios from "axios";
+import store from "./data/store";
+
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      store,
+    };
+  },
   components: {
     AppHeader,
     AppMain,
   },
+  methods: {
+    getCharacters() {
+      axios
+        .get(store.apiUrlBrBa)
+        .then((result) => {
+          console.log(result.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.getCharacters();
+  },
 };
 </script>
 <template>
-  <AppHeader />
+  <AppHeader title="Breaking Bad API App" />
   <main>
     <AppMain />
   </main>
